16a37e4c1743a73570eb6623825152b7517e42dc
RachidaBf <boufos.rachida@gmail.com>
2023-02-23 07:31:16 -0800

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper ->  checks for uppercase character
+ * @c: char type letter
+ *
+ * Return: 1 if uppercase, 0 if not uppercase
+ */
+int _isupper(int c)
+{
+
+
+	if (c > 64 && c < 91)
+		return (1);
+	else
+		return (0);
+}
