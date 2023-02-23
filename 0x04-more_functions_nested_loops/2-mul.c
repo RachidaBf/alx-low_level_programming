1b341b8069d2c7cc050b0413742ac774ef5951b6
RachidaBf <boufos.rachida@gmail.com>
2023-02-23 10:09:07 -0800

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * mul -> Mult two int
+ *
+ * @a: First int
+ * @b: Second int
+ *
+ * Return: res of a, b
+ */
+
+int mul(int a, int b)
+{
+	int res = a * b;
+
+
+	return (res);
+}
