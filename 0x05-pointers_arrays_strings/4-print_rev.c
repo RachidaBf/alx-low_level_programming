f9f5b9b25843d0d08291856dd5b01d5532cab11b
RachidaBf <boufos.rachida@gmail.com>
2023-02-27 12:32:40 -0800

    print-rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,9 +11,9 @@ void print_rev(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 		;
 
-	for (i--; s[i] >= 0; i--)
-	
+	for (i = i - 1; s[i] >= 0; i--)
+	{
 		_putchar(s[i]);
-		
-		_putchar('\n');
+	}
+	_putchar('\n');
 }
