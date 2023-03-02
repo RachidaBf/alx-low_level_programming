ec9d0eb2fd48845f671635e7b7b000d918f293b4
RachidaBf <boufos.rachida@gmail.com>
2023-03-02 15:43:23 -0800

    string-toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,9 +11,10 @@ char *string_toupper(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
+		if (str[i] >= 97 && str[i] <= 122)
 		{
 			str[i] = str[i] - 32;
 		}
 	}
+	return (char);
 }
