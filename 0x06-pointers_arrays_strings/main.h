47be694fcf983dae4440201eb0c5938fd25a20da
RachidaBf <boufos.rachida@gmail.com>
2023-03-03 15:02:59 -0800

    main

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -11,7 +11,6 @@ char *string_toupper(char *);
 char *leet(char *);
 char *_strncat(char *dest, char *src, int n);
 char *cap_string(char *);
-int _putchar(char c);
 void print_number(int n);
 
 
