ab78f3e5ee669695c8a456d2c67dc372dd1592bf
RachidaBf <boufos.rachida@gmail.com>
2023-03-03 14:28:21 -0800

    print_number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -20,9 +20,9 @@ void print_number(int n)
 	}
 	a = n;
 
-	if (n / 10)
+	if (a / 10)
 	{
-		print_number(n / 10);
-		_putchar(n % 10 + '0');
+		print_number(a / 10);
+		_putchar(a % 10 + '0');
 	}
 }
