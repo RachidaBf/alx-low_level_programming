0e896baa8fd8a536e27db5e40f5dfff141098cf9
RachidaBf <boufos.rachida@gmail.com>
2023-03-03 18:50:27 -0800

    print_buffer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,5 +13,5 @@ char *_strncat(char *dest, char *src, int n);
 char *cap_string(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-
+void print_buffer(char *b, int size);
 #endif
