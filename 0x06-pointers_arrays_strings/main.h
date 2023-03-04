9f706233b602bf6139775700aef4ec1e424107fc
RachidaBf <boufos.rachida@gmail.com>
2023-03-03 18:48:36 -0800

    infinite_add

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,6 +12,6 @@ char *leet(char *);
 char *_strncat(char *dest, char *src, int n);
 char *cap_string(char *);
 void print_number(int n);
-
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif
