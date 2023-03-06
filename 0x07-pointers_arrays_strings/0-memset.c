312a5246b6b950fa4e5563f527d96a2c8ced620b
RachidaBf <boufos.rachida@gmail.com>
2023-03-06 04:20:25 -0800

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,11 +10,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; s[i] != '\0'; i++)
-
-		if (s[i] == n)
-		{
-			b = n;
-		}
+	for (i = 0; i < n; i++)
+		s[i] = b;
 	return (s);
 }
