48c592d26cce629b27e2f854cd769ae5f2c164cf
RachidaBf <boufos.rachida@gmail.com>
2023-04-04 06:44:44 -0700

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t i = 0;
+	size_t nodes = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		i++;
+		nodes;
 	}
-	return (i);
+	return (nodes);
 }
 
