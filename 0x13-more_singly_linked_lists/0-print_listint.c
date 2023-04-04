3956186a16208141a1b44a5a24968fc1086a806d
RachidaBf <boufos.rachida@gmail.com>
2023-04-04 00:10:39 -0700

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint -> prints all element of listint_t list
+ * @h:head of a list
+ * Return: numbers of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,4 +1,5 @@
-#ifndef #define LISTS_H
+#ifndef _LISTS_H_
+#define _LISTS_H_
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,3 +19,7 @@ typedef struct listint_s
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+
+
+
+#endif
