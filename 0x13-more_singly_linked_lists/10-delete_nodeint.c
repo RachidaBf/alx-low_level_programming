6c55a3e6e1f00176bebd132a542d96bee730a9af
RachidaBf <boufos.rachida@gmail.com>
2023-04-05 18:12:00 -0700

    delete

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -28,7 +28,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			return (-1);
 		T = T->next;
 	}
-	T = temp->next;
+	T = T->next;
 	T->next = next->next;
 	free(next);
 	return (1);
